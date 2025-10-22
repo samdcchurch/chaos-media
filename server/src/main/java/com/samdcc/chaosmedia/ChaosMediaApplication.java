89e462530061a77884e05c64ffca22d17d1ec183
Samuel Church <sam.dc.church@gmail.com>
2025-10-22 16:50:55 -0400

    Server: Refactor test names to match rest of src

--- server/src/main/java/com/samdcc/chaosmedia/ChaosMediaApplication.java
+++ server/src/main/java/com/samdcc/chaosmedia/ChaosMediaApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChaosMediaApp {
+public class ChaosMediaApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ChaosMediaApp.class, args);
+		SpringApplication.run(ChaosMediaApplication.class, args);
 	}
 }
--- server/src/test/java/com/samdcc/chaosmedia/ChaosMediaApplicationTests.java
+++ server/src/test/java/com/samdcc/chaosmedia/ChaosMediaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.samdcc.chaosmedia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ChaosMediaApplicationTests {
 
 	@Test
 	void contextLoads() {
